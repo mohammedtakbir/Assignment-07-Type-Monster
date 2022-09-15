47a484395a1b3c52c68b07e5898d81e2cf562fc7
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-15 07:01:37 +0600

    problem with count 0 resolved

--- script.js
+++ script.js
@@ -108,19 +108,19 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-
+    
     // finished timer
-    if (count == 0) {
+    if (count === 0) {
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
-      countdownOverlay.style.display = "flex";
+      countdownOverlay.style.display = "none";
       display.classList.remove("inactive");
-
+      
       clearInterval(startCountdown);
       startTime = new Date().getTime();
     }
     count--;
-  }, 1000);
+  }, 500);
 };
 
 // START Countdown
