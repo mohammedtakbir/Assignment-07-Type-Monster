98bfd552d78a81165fdab8fb69d7d97fde40998e
mohammedtakbir <takbirhossain678@gmail.com>
2022-09-15 08:17:36 +0600

    added 3 columns in desktop view and 1 column in mobile view to the history part

--- history.js
+++ history.js
@@ -30,9 +30,9 @@ function displayHistory() {
     newRow.classList.add("card");
 
     newRow.innerHTML = `
-  <h3>${test.questionText}</h3>
-  <p>You took: <span class="bold">${test.timeTaken}</span> seconds</p>
-    <p>You made <span class="bold red">${test.errorCount}</span> mistakes</p>
+  <h3 style="margin: 10px 0; text-align: center">${test.questionText}</h3>
+  <p style="margin: 10px 0; text-align: center">You took: <span class="bold">${test.timeTaken}</span> seconds</p>
+  <p style="text-align: center">You made <span class="bold red">${test.errorCount}</span> mistakes</p>
   `;
 
     histories.appendChild(newRow);
